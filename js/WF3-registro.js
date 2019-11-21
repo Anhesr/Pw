d4b20dce3e2d5b31fb2841e4e04be5e0c050c0fd
Janthonyo <gran-leon@hotmail.es>
2019-11-21 18:54:42 +0100

    Cambios en el registro

--- js/WF3-registro.js
+++ js/WF3-registro.js
@@ -1,3 +1,33 @@
 function accederListaPubli() {
 	window.location = "../html/WF5-lista_publicaciones.html";
 }
+
+function validarPass() {
+	var password = document.getElementById("pass");
+	var msjError = document.getElementById("msjErrorValidar");
+
+	if (password.value.length >= 7)
+		msjError.style.display = "none";
+	else
+		msjError.style.display = "unset";
+}
+
+function verificarPass() {
+	var firstPass = document.getElementById("pass");
+	var secondPass = document.getElementById("pass2")
+	var boton = document.getElementById("botonRegistro")
+	var msj = document.getElementById("msjErrorVerificar")
+
+	if (firstPass.value === secondPass.value)
+	{
+		msj.style.display = "none";
+		boton.disabled = false;
+		
+	}
+	else
+	{
+		msj.style.display = "unset";
+		boton.disabled = true;
+	}
+	
+}
